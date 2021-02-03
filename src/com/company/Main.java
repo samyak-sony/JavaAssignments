fea26fdc150a1684c41212214fa1676f71074304
unknown <sony.samyak@gmail.com>
2021-02-03 22:44:07 +0530

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,142 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	
+	    Scanner sc = new Scanner(System.in);
+
+	    /*
+	    Question 1
+	     */
+
+
+//	    System.out.println("Enter your credits");
+//	    int credits = sc.nextInt();
+//
+//	    if(credits >= 7500){
+//            System.out.println("Tera leader");
+//        }else if(credits >= 6000 && credits < 7500){
+//            System.out.println("Gega leader");
+//        }else if(credits >= 4500 && credits < 6000){
+//            System.out.println("Mega leader");
+//        }else{
+//            System.out.println("Rising star");
+//        }
+
+
+        //PSEUDOCODE
+        /*
+        Take input credits
+        if credits >= 7500:
+            print"Tera leader"
+        end if
+
+        else if credits >= 6000 and credits < 7500:
+            print"Gega leader"
+        end else if
+
+        else if credits >= 4500 and credits < 6000:
+            print"Mega leader"
+        end else if
+
+        else:
+            print"Rising star"
+        end else
+
+         */
+
+
+
+
+        /*
+        Question 2
+         */
+
+//        System.out.println("Enter amount");
+//        int amount = sc.nextInt();
+//        System.out.println("Enter rate");
+//        int rate = sc.nextInt();
+//        System.out.println("Enter time");
+//        int time = sc.nextInt();
+//
+//        double si;
+//        si = (amount*rate*time)/100.0;
+//        System.out.println("Simple interest = "+si);
+
+
+        //PSEUDOCODE
+
+        /*
+
+        Take input amount
+        Take input rate
+        Take input time
+        declare si
+        si:= (amount*rate*time)/100.0
+        print si
+
+         */
+
+
+
+        //Question 3
+//
+//        System.out.println("Enter first number");
+//        int n1 = sc.nextInt();
+//        System.out.println("Enter second number");
+//        int n2 = sc.nextInt();
+//
+//        while(n1 != n2){
+//
+//            if(n2 > n1){
+//                n2 = n2 - n1;
+//            }else{
+//                n1 = n1 - n2;
+//            }
+//        }
+//
+//        System.out.println("GCD is "+ n1);
+
+    //PSEUDOCODE
+
+    /*
+
+
+    Take input first number
+    Take input second number
+    while n1 != n2:
+        if n2 > n2:
+            n2 = n2 - n1
+        end if
+        else:
+            n1 = n1 - n2
+        end else
+    end while
+    print gcd of the two numbers.
+
+
+     */
+
+
+
+    //QUESTION 4
+
+    int n = 10;
+
+
+       //System.out.println("J value at last"+j);
+   }
+
+
+
+
+
+
+
+
     }
+
+
 }
